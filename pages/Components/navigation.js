3ef31af64db68ef95fdb4c3fa0f6ca94990ab9ae
Nijumdip <nijumdip019911@gmail.com>
2022-11-26 10:35:20 +0600

    error solved

--- pages/Components/navigation.js
+++ pages/Components/navigation.js
@@ -3,26 +3,26 @@ import { Navbar } from "flowbite-react";
 function Navigation() {
     return (
         <>
-            <Navbar fluid={true} rounded={true} >
-                <Navbar.Brand href="https://flowbite.com/">
+            
+            <Navbar className="py-5 bg-transparent z-40" >
+                <Navbar.Brand href="/">
                     <img
-                        src="https://flowbite.com/docs/images/logo.svg"
-                        className="mr-3 h-6 sm:h-9"
-                        alt="Flowbite Logo"
+                        src="https://i.ibb.co/d0dBDFB/download.jpg"
+                        className="mr-3 h-10 sm:h-16 rounded-full p-2"
+                        alt="programming logo"
                     />
                     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                        Flowbite
+                        Shamima Akter
                     </span>
                 </Navbar.Brand>
+
                 <Navbar.Toggle />
                 <Navbar.Collapse>
-                    <Navbar.Link href="/navbars" active={true}>
-                        Home
-                    </Navbar.Link>
-                    <Navbar.Link href="/navbars">About</Navbar.Link>
-                    <Navbar.Link href="/navbars">Services</Navbar.Link>
-                    <Navbar.Link href="/navbars">Pricing</Navbar.Link>
-                    <Navbar.Link href="/navbars">Contact</Navbar.Link>
+                    <Navbar.Link href="/"> Home </Navbar.Link>
+                    <Navbar.Link href="/">About</Navbar.Link>
+                    <Navbar.Link href="/">Services</Navbar.Link>
+                    <Navbar.Link href="/">Pricing</Navbar.Link>
+                    <Navbar.Link href="/">Contact</Navbar.Link>
                 </Navbar.Collapse>
             </Navbar>
         </>
