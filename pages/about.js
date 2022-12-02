910e62593ef94ba40ba7e81153e43cf9a5a3cfdc
Nijumdip <nijumdip019911@gmail.com>
2022-12-02 14:40:50 +0600

    add image

--- pages/about.js
+++ pages/about.js
@@ -6,18 +6,19 @@ function About() {
         <>
             <div class="px-6 mx-6 rounded-2xl py-12 md:px-12 bg-gray-50 text-gray-800 text-center lg:text-left ">
                 <div class="container mx-auto xl:px-20 ">
-                    <div class="grid lg:grid-cols-2 gap-12 ">
+                    <div class="grid lg:grid-cols-2  ">
                         <div class="mb-12 lg:mb-0">
-                            <h3 class="text-xl leading-5 dark:text-white font-semibold text-gray-800 lg:mt-10 mt-8">
-                                About Us <hr />
-                            </h3> <br />
+                            <h3 class="text-4xl leading-5 dark:text-white font-semibold text-gray-800 mt-8 ">
+                                About Us 
+                            </h3>{" "}
+                            <hr className="mt-5 w-80" /><br />
                             <img
-                                src="https://mdbootstrap.com/img/new/ecommerce/vertical/028.jpg"
-                                class="w-full rounded-lg shadow-lg"
+                                src="https://i.ibb.co/4VRSWvV/nijum-pic-removebg-preview.png"
+                                class="w-80  rounded-lg shadow-lg"
                                 alt=""
                             />
                         </div>
-                        <div>
+                        <div class="mt-20  lg:mb-0">
                             <h1>Who am i?</h1>
                             <p>
                                 I'm Front-end Developer from Dhaka, Bangladesh, working in web
