96ef8ac512a75e48a85b50180eab8486ceb70676
Nijumdip <nijumdip019911@gmail.com>
2022-12-06 17:23:24 +0600

    added about card

--- Components/aboutCard.js
+++ Components/aboutCard.js
@@ -1,20 +1,19 @@
 import React from "react";
 
-function AboutCard() {
+const AboutCard = () => {
     return (
-        <a
-            href="#"
-            class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-        >
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-            </h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
-                chronological order.
-            </p>
-        </a>
+        <>
+            <div class="max-w-sm p-6 bg-pink-100 rounded-lg shadow-md hover:bg-gray-100 ">
+                <h5 class="mb-2 text-2xl font-bold">
+                    Web Design
+                </h5>
+                <p class="font-normal text-gray-700 mb-3">
+                    web design, web development, app development, figma to html,existing site redevelopment
+                    
+                </p>
+            </div>
+        </>
     );
-}
+};
 
 export default AboutCard;
